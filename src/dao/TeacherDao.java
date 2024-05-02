4f8dad0406fcad76d770e94906aeaa7a4fea3a30
2374551 <2374551@DESKTOP-DMGA9BU>
2024-05-02 12:15:04 +0900

    コードの修正

---
+++ src/dao/TeacherDao.java
@@ -0,0 +1,14 @@
+package dao;
+
+import bean.Teacher;
+
+public class TeacherDao extends Dao{
+	//メソッド
+	public Teacher get (String id) throws Exception {
+
+	}
+
+	public Teacher get (String id,String password) throws Exception {
+
+	}
+}
