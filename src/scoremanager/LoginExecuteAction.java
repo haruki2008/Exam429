daec167f00e3224336284547b0b0621c23bf72c7
yuki <yamani.yuki8412@gmail.com>
2024-05-10 10:51:08 +0900

    修正希望

--- src/scoremanager/LoginExecuteAction.java
+++ src/scoremanager/LoginExecuteAction.java
@@ -1,64 +1,56 @@
 package scoremanager;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import bean.School;
 import bean.Teacher;
-import dao.TeacherDao;
 import tool.Action;
 
-public class LoginExecuteAction extends Action{
+public class LoginExecuteAction extends Action {
 
 	@Override
-	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		//ローカル変数の宣言 1
-		String url = "";
-
+	public void execute(
+		HttpServletRequest req, HttpServletResponse res
+	) throws Exception{
+		// ローカル変数の宣言 1
 		Teacher teacher = new Teacher();
-		TeacherDao tDao = new TeacherDao();
+		School school = new School();
 
-		//リクエストパラメータ―の取得 2
+		// リクエストパラメータ 2
 		String id = req.getParameter("id");
 		String password = req.getParameter("password");
+		String name = req.getParameter("namae");
+		String school_cd = req.getParameter("school_cd");
 
-		//DBからデータ取得 3
-		teacher = tDao.login(id,password);
+		// DBからデータ取得 3
+		//
 
-		//ビジネスロジック 4
+		// ビジネスロジック 4
+		teacher.setId(id);
+		teacher.setPassword(password);
+		teacher.setName(name);
 
+		school.setCd(school_cd);
+		school.setName("大宮校");
 
+		teacher.setSchool(school); //school型
 
-		if (teacher != null) {
-			// 認証済みフラグを立てる
-			teacher.setAuthenticated(true);
-			//Sessionを有効にする
-			HttpSession session = req.getSession(true);
-			//セッションに"user"という変数名で値はTeacher変数の中身
-			session.setAttribute("user", teacher);
-			//リダイレクト
-			url = "main/Menu.action";
-			res.sendRedirect(url);
-		} else {
-			Map<String, String> errors = new HashMap<>();// エラーメッセージ
-			errors.put("1", "ログインに失敗しました。IDまたはPasswardが違います。");
-			req.setAttribute("errors", errors);
-			url = "login.jsp";
-			req.getRequestDispatcher(url).forward(req, res);
-		}
+		// sessionを有効化
+		HttpSession session = req.getSession(true);
 
+		// セッションに"user"という変数名、値はTeacher変数の中身
+		session.setAttribute("user", teacher);
 
-		//DBへデータ保存 5
-		//なし
-		//レスポンス値をセット 6
-		//なし
-		//JSPへフォワード 7
-		//req.getRequestDispatcher("main/Menu.action").forward(req, res);
+		// DBへデータ保存 5
+		//
+		// レスポンス値をセット 6
+		//
+		// JSPへフォワード 7
+		//
 
-
+		req.getRequestDispatcher("main/Menu.action").forward(req, res);
 	}
 
 }
